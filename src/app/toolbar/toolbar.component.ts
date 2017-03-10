fe1fbdafa2c636b55e961f5433a90022efce9a42
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2017-03-10 15:08:09 +0100

    logging search term on search click

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -9,7 +9,13 @@ import {
   templateUrl: './toolbar.component.html'
 })
 export class ToolbarComponent implements OnInit {
+  public searchTerm: string;
+
   public ngOnInit() {
     console.log('ToolbarComponent init');
+  }
+
+  public search() {
+    console.log(this.searchTerm);
   }
 }
