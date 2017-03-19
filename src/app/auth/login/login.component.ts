633e06f9c17a3f7b21c6e3c46d5a3d00d056d72c
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-03-19 20:50:27 +0100

    added login action to login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,13 +3,23 @@ import {
   OnInit
 } from '@angular/core';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'c-login',
   styleUrls: [ './login.component.scss' ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+  ) {}
+
   public ngOnInit() {
     console.log('LoginComponent init');
+  }
+
+  public login() {
+    this.authService.login('lol', 'mao');
   }
 }
