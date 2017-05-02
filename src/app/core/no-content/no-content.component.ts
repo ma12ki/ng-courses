d5177509277e58178948326549cc35ee08da109f
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-05-02 10:15:22 +0200

    updated 404 page

--- src/app/core/no-content/no-content.component.ts
+++ src/app/core/no-content/no-content.component.ts
@@ -3,8 +3,10 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'no-content',
   template: `
-    <div>
-      <h1>404: page missing</h1>
+    <div style="text-align: center">
+      <h1>Congratulations, you have found the secret page!</h1>
+      <h3>Here, have a cake as a reward:</h3>
+      <md-icon style="width: 60px; height: 60px; font-size: 60px">cake</md-icon>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
