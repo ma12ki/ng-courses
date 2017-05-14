f5021bfbeb8a5d952b7ea266ab89798f476b617e
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-05-14 14:41:01 +0200

    auth effects and service cleanup

--- src/app/core/auth/auth.effects.ts
+++ src/app/core/auth/auth.effects.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Router } from '@angular/router';
 import { Actions, Effect, toPayload } from '@ngrx/effects';
 import { Observable } from 'rxjs/Observable';
 
 import { AuthService } from './auth.service';
 import * as auth from './auth.actions';
+import { remove as removeAuthStorage } from './auth.storage';
 import { IUserAndToken, IUserCredentials } from '../../shared/user.entity';
+import { LoaderService } from '../../shared/loader';
 
 @Injectable()
 export class AuthEffects {
   constructor(
     private actions$: Actions,
     private authService: AuthService,
+    private loaderService: LoaderService,
+    private router: Router,
   ) { }
 
   // tslint:disable-next-line:member-ordering
@@ -19,8 +24,23 @@ export class AuthEffects {
   public login$ = this.actions$
     .ofType(auth.LOGIN_START)
     .map(toPayload)
-    .switchMap((credentials: IUserCredentials) => this.authService.reduxLogin(credentials))
+    .do(() => this.loaderService.show())
+    .switchMap((credentials: IUserCredentials) => this.authService.login(credentials))
       .map((res: IUserAndToken) => new auth.LoginSuccessAction(res))
+      .do(() => this.router.navigateByUrl(''))
       .catch((err) => Observable.of(new auth.LoginErrorAction(err))
-    );
+    )
+    .do(() => this.loaderService.hide());
+
+  // tslint:disable-next-line:member-ordering
+  @Effect()
+  public logout$ = this.actions$
+    .ofType(auth.LOGOUT_START)
+    .do(() => this.loaderService.show())
+    .delay(500)
+    .do(() => removeAuthStorage())
+    .map(() => new auth.LogoutSuccessAction())
+    .do(() => this.router.navigateByUrl('/login'))
+    .catch((err) => Observable.of(new auth.LogoutErrorAction(err)))
+    .do(() => this.loaderService.hide());
 }
--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,66 +1,23 @@
 import { Injectable, Inject } from '@angular/core';
 import { Http, RequestOptionsArgs, ResponseContentType } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs';
 import 'rxjs';
-import * as faker from 'Faker';
 import * as decode from 'jwt-decode';
 
 import { API_URL } from './../../app.constants';
 import { IUser, IUserAndToken, IUserCredentials } from '../../shared/user.entity';
-import { AuthorizedHttp } from '../authorized-http.service';
 
 @Injectable()
 export class AuthService {
-  public static TOKEN_STORAGE_KEY = 'AUTH_TOKEN';
-  public static USER_STORAGE_KEY = 'AUTH_USER';
-  private _hasToken: boolean;
-  private _hasToken$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private _userInfo$: BehaviorSubject<IUser> = new BehaviorSubject<IUser>(null);
   private _loginEndpoint: string = 'login';
   private _usersEndpoint: string = 'users';
 
   constructor(
     @Inject(API_URL) private API_URL: string,
     private http: Http,
-  ) {
-    this.updateHasToken();
-    this.updateUserInfo();
-  }
+  ) { }
 
-  public get isAuthenticated(): boolean {
-    return this._hasToken;
-  }
-
-  public get isAuthenticated$(): Observable<boolean> {
-    return this._hasToken$.asObservable();
-  }
-
-  public login(login: string, password: string): Observable<IUser> {
-    const options: RequestOptionsArgs = {
-      responseType: ResponseContentType.Json,
-    };
-
-    return this.http.get(`${this.API_URL}${this._loginEndpoint}`, options)
-      .map((response) => response.json())
-      .map(({ token }) => {
-        localStorage.setItem(AuthService.TOKEN_STORAGE_KEY, token);
-        this.updateHasToken();
-        return token;
-      })
-      .switchMap((token) => {
-        const { id } = decode(token);
-        return this.http.get(`${this.API_URL}${this._usersEndpoint}/${id}`, options);
-      })
-      .map((response) => response.json())
-      .map((user) => {
-        localStorage.setItem(AuthService.USER_STORAGE_KEY, JSON.stringify(user));
-        this.updateUserInfo();
-        return user;
-      });
-  }
-
-  public reduxLogin(credentials: IUserCredentials): Observable<IUserAndToken> {
+  public login(credentials: IUserCredentials): Observable<IUserAndToken> {
     const options: RequestOptionsArgs = {
       responseType: ResponseContentType.Json,
     };
@@ -82,35 +39,5 @@ export class AuthService {
         ret.user = user;
         return ret;
       });
-  }
-
-  public logout(): Observable<null> {
-    localStorage.removeItem(AuthService.USER_STORAGE_KEY);
-    localStorage.removeItem(AuthService.TOKEN_STORAGE_KEY);
-
-    return Observable.of(null)
-      .delay(1000)
-      .do(() => console.info('successfully logged out!'))
-      .do(() => {
-        this.updateHasToken();
-        this.updateUserInfo();
-      });
-  }
-
-  public get userInfo(): IUser {
-    return JSON.parse(localStorage.getItem(AuthService.USER_STORAGE_KEY));
-  }
-
-  public get userInfo$(): Observable<IUser> {
-    return this._userInfo$.asObservable();
-  }
-
-  private updateHasToken(): void {
-    this._hasToken = !!localStorage.getItem(AuthService.TOKEN_STORAGE_KEY);
-    this._hasToken$.next(this._hasToken);
-  }
-
-  private updateUserInfo(): void {
-    this._userInfo$.next(JSON.parse(localStorage.getItem(AuthService.USER_STORAGE_KEY)));
   }
 }
