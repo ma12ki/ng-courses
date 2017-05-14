a98c31a2391ef88753724f676b4f60897fbbc92b
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-05-14 14:41:35 +0200

    used ngrx in nav

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,15 +1,14 @@
 import {
   Component,
-  OnInit,
   ChangeDetectionStrategy,
-  OnDestroy,
 } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
-import { Subscription } from 'rxjs/Subscription';
+import { Store } from '@ngrx/store';
 
-import { AuthService } from '../auth/auth.service';
 import { LoaderService } from './../../shared/loader/loader.service';
+import { authSelectors, State } from '../../app.reducer';
+import { LogoutStartAction } from '../auth/auth.actions';
 
 @Component({
   selector: 'c-nav',
@@ -17,36 +16,22 @@ import { LoaderService } from './../../shared/loader/loader.service';
   templateUrl: './nav.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavComponent implements OnInit, OnDestroy {
+export class NavComponent {
   public isUserAuthenticated$: Observable<boolean>;
-  private _subscriptions: Subscription[] = [];
 
   constructor(
     private router: Router,
-    private authService: AuthService,
     private loaderService: LoaderService,
+    private store: Store<State>,
   ) {
-    this.isUserAuthenticated$ = authService.isAuthenticated$;
-  }
-
-  public ngOnInit() {
-    console.log('NavComponent init');
+    this.isUserAuthenticated$ = this.store.select(authSelectors.isAuthenticated);
   }
 
   public logout() {
-    this.loaderService.show();
-    this._subscriptions.push(
-      this.authService.logout()
-        .do(() => this.loaderService.hide())
-        .do(() => this.router.navigate(['/login']))
-        .subscribe());
+    this.store.dispatch(new LogoutStartAction());
   }
 
   public navigate(link: string) {
     this.router.navigateByUrl(link);
-  }
-
-  public ngOnDestroy(): void {
-    this._subscriptions.forEach((s) => s.unsubscribe());
   }
 }
