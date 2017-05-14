f406f9e0b501b4c558f1fe96ba85451dab67ca65
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-05-14 14:41:16 +0200

    used ngrx in auth guard

--- src/app/core/auth/auth-guard.service.ts
+++ src/app/core/auth/auth-guard.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+import { Store } from '@ngrx/store';
 
-import { AuthService } from './auth.service';
+import { authSelectors, State } from '../../app.reducer';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
+  private isAuthenticated = false;
 
   constructor(
     private router: Router,
-    private authService: AuthService,
-  ) { }
+    private store: Store<State>
+  ) {
+    this.store.select(authSelectors.isAuthenticated)
+      .subscribe((isAuthenticated) => {
+        this.isAuthenticated = isAuthenticated;
+      });
+  }
 
   public canActivate(route: ActivatedRouteSnapshot) {
-    if (!this.authService.isAuthenticated) {
+    if (!this.isAuthenticated) {
       this.router.navigate(['/login']);
       return false;
     }
